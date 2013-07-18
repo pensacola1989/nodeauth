fa95f12c3a1511d2a5db2ee0f57282d4284fa4d5
fuck <qindongwoxin@gmail.com>
2013-07-18 22:30:30 +0800

    update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,28 @@
 var Site = Site || {};
 Site.View = Site.View || {};
+Site.Model = Site.Model || {};
+
+
+Site.Model.user = Backbone.Model.extend({
+	defaults: {
+		name: 'www',
+		age: 24
+	}
+});
+
 Site.Router = Backbone.Router.extend({
 	routes: {
 		'': 'index',
 		'login': 'login',
-		'user': 'users'		
+		'user': 'users',
+		'person': 'person'
 	},
 
 	views: {},
 
 	initialize: function () {
 		_.bindAll(this,'index','login','person');
-		this.views.index = new Site.View.index();
+		this.views.index = new Site.View.index({ model: new Site.Model.user() });
 		this.views.login = new Site.View.login();
 		this.views.person = new Site.View.person();
 
@@ -25,11 +36,18 @@ Site.Router = Backbone.Router.extend({
 	},
 
 	person: function () {
+		if(!Site.user) {
+			this.navigate('',true);
+			return;
+		}
 		this.view = this.views.person;
 		this.view.render();
 	},
 
 	login: function () {
+		if(Site.user) {
+			(new Site.View.index).render();	
+		}
 		this.view = this.views.login;
 		this.view.render();
 	}
@@ -37,6 +55,11 @@ Site.Router = Backbone.Router.extend({
 });
 
 Site.View.index = Backbone.View.extend({
+
+	initialize: function () {
+
+		// console.log(this.model.get('name'));	
+	},
 
 	el: '.container',
 
@@ -58,7 +81,22 @@ Site.View.index = Backbone.View.extend({
 
 Site.View.login = Backbone.View.extend({
 
+	initialize: function () {
+		_.bindAll(this,'getLogin');
+
+	},
+
 	el: '.container',
+
+	events: {
+		'click #submit': 'getLogin'
+	},
+
+	getLogin: function () {
+		Site.user = { name: 'www', sex: 25 };
+		(new Site.View.person).render();
+
+	},
 
 	render: function () {
 		var temp = $('#login').html();
@@ -69,10 +107,23 @@ Site.View.login = Backbone.View.extend({
 
 Site.View.person = Backbone.View.extend({
 
+	el: '.container',
+
+	initialize: function () {
+			
+	},
+
+	render: function () {	
+		$(this.el).html('<h1>this is a personal Page!');
+	}
 });
 
+Site.init = function () {
 
+	Site.router = new Site.Router();
+	Backbone.history.start();
+	// Site.user = { name: 'www', age: 25 };
+}
 $(document).ready(function () {
-	var router = new Site.Router();
-	Backbone.history.start({ pushState: true });
+	Site.init();
 });
